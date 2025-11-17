3dd547143e01e343901c57a880846d388af21ce0
wbhaoshuai <1402078577@qq.com>
2025-11-17 17:29:41 +0800

    Added the function of jumping from PlayerActivity to Leaderboard Acticity.

--- app/src/main/java/com/example/assignmenttwo/PlayerActivity.java
+++ app/src/main/java/com/example/assignmenttwo/PlayerActivity.java
@@ -1,6 +1,8 @@
 package com.example.assignmenttwo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,4 +23,10 @@ public class PlayerActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    public void onclickSubmit(View view){
+        Intent intent = new Intent(this, LeaderboardActivity.class);
+        startActivity(intent);
+    }
+
 }
