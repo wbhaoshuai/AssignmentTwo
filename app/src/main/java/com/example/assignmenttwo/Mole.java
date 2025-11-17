6a1246647ffe8ab11368d276d8421b17d5fa75a2
wbhaoshuai <1402078577@qq.com>
2025-11-17 20:18:23 +0800

    Created a constructor for Mole

--- app/src/main/java/com/example/assignmenttwo/Mole.java
+++ app/src/main/java/com/example/assignmenttwo/Mole.java
@@ -1,4 +1,18 @@
 package com.example.assignmenttwo;
 
+import android.widget.ImageView;
+
 public class Mole {
+
+    private int index;
+    private ImageView imageView;
+    private  boolean isVisible;
+
+    // constructor
+    public Mole(int index, ImageView imageView){
+        this.index = index;
+        this.imageView = imageView;
+        this.isVisible = false;
+    }
+
 }
