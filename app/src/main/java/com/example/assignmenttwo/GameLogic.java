d020c3d0d4fc0f93dbfdc2541ba2683c66de6814
wbhaoshuai <1402078577@qq.com>
2025-11-17 20:37:43 +0800

    Added 14 private attributes

--- app/src/main/java/com/example/assignmenttwo/GameLogic.java
+++ app/src/main/java/com/example/assignmenttwo/GameLogic.java
@@ -1,4 +1,28 @@
 package com.example.assignmenttwo;
 
+import android.content.Context;
+import android.os.CountDownTimer;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.logging.Handler;
+
 public class GameLogic {
+
+    private long MOLE_DISPLAY_TIME;
+    private long GAME_DURATION;
+    private  int currentScore;
+    private int timeRemaining;
+    private int currentMoleIndex;
+    private Handler moleHandler;
+    private  Runnable moleRunnable;
+    private CountDownTimer gameTimer;
+    private Random random;
+    private ArrayList<Mole> moles;
+    private TextView scoreTextView;
+    private  TextView timerTextView;
+    private boolean isGameRunning;
+    private Context context;
+    
 }
