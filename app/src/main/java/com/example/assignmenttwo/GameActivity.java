0d4a50216e2706b9dff3a6625aea2bf7b2dd0645
wbhaoshuai <1402078577@qq.com>
2025-11-17 16:50:55 +0800

    Added the function of jumping from GameActivity to PlayerActivity after the countdown ends

--- app/src/main/java/com/example/assignmenttwo/GameActivity.java
+++ app/src/main/java/com/example/assignmenttwo/GameActivity.java
@@ -1,6 +1,9 @@
 package com.example.assignmenttwo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,23 @@ public class GameActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView tv_time = findViewById(R.id.tv_timer_text);
+
+        new CountDownTimer(30000, 1000) { // Total time, tick interval
+            public void onTick(long millisUntilFinished) {
+                // Update the timer TextView
+                int remainingSeconds = (int) (millisUntilFinished / 1000);
+                tv_time.setText(getString(R.string.timer_text, remainingSeconds));
+            }
+            public void onFinish() {
+                // End the game and move to the next screen
+                tv_time.setText(R.string.Timer);
+                Intent intent = new Intent(GameActivity.this, PlayerActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }.start();
     }
+
 }
