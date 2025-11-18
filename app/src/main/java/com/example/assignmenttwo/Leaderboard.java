612fd57438b9cae2a63ffc85b0fe756b44c704c1
wbhaoshuai <1402078577@qq.com>
2025-11-18 19:54:09 +0800

    Created Leaderboard class and constructor

--- app/src/main/java/com/example/assignmenttwo/Leaderboard.java
+++ app/src/main/java/com/example/assignmenttwo/Leaderboard.java
@@ -1,4 +1,18 @@
 package com.example.assignmenttwo;
 
+import java.util.ArrayList;
+
 public class Leaderboard {
+
+    private Leaderboard leaderboardInstance;
+    private ArrayList<Player> leaderboard;
+    private final int MAX_LEADERBOARD_SIZE = 5;
+
+    // constructor
+    private Leaderboard(){
+
+    }
+
+
+
 }
