233930b1e5d9d21c5ba17750c8d01f4d67e47deb
wbhaoshuai <1402078577@qq.com>
2025-11-18 20:17:35 +0800

    UI initialized and loaded for PlayerActivity

--- app/src/main/java/com/example/assignmenttwo/PlayerActivity.java
+++ app/src/main/java/com/example/assignmenttwo/PlayerActivity.java
@@ -3,6 +3,9 @@ package com.example.assignmenttwo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class PlayerActivity extends AppCompatActivity {
+
+    private TextView tv_playerscore;
+    private EditText et_playername;
+    private RadioGroup rg_avatar;
+    private int finalScore;
+    private Leaderboard leaderboardInstance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,12 @@ public class PlayerActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Initialize UI components
+        tv_playerscore = findViewById(R.id.tv_playerscore);
+        et_playername = findViewById(R.id.et_playername);
+        rg_avatar = findViewById(R.id.rg_avator);
+
     }
 
     public void onclickSubmit(View view){
