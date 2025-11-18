7404628f20ded13b629dd4c183736c4d37a4548a
wbhaoshuai <1402078577@qq.com>
2025-11-18 20:02:48 +0800

    Created two methods in the Leaderboard class

--- app/src/main/java/com/example/assignmenttwo/Leaderboard.java
+++ app/src/main/java/com/example/assignmenttwo/Leaderboard.java
@@ -1,6 +1,7 @@
 package com.example.assignmenttwo;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Leaderboard {
 
@@ -13,6 +14,27 @@ public class Leaderboard {
 
     }
 
+    // Get singleton instance of Leaderboard
+    public Leaderboard getInstance() {
+        return leaderboardInstance;
+    }
+
+    // Update leaderboard with new player, maintaining size and sorting
+    public void updateLeaderboard(Player newPlayer) {
+        // Add new player
+        leaderboard.add(newPlayer);
+        // Sort players by score in descending order
+        leaderboard.sort(new Comparator<Player>() {
+            @Override
+            public int compare(Player p1, Player p2) {
+                return Integer.compare(p2.getPlayerScore(), p1.getPlayerScore());
+            }
+        });
+        // Ensure leaderboard doesn't exceed max size
+        while (leaderboard.size() > MAX_LEADERBOARD_SIZE) {
+            leaderboard.remove(leaderboard.size() - 1);
+        }
+    }
 
 
 }
